1679e7fe65ff351174b173947b9fd1053e451ee7
alexroverandom <alexroverandom@gmail.com>
2015-11-28 20:00:15 +0200

    add new api controllers

---
+++ Taxi.WebApp/Controllers/Api/CarBrandController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Taxi.DataAccess;
+
+namespace Taxi.WebApp.Controllers.Api
+{
+    public class CarBrandController : ApiBaseController
+    {
+		public CarBrandController(EntityContext context)
+			: base(context)
+		{ }
+
+    }
+}
---
+++ Taxi.WebApp/Controllers/Api/CarModelController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Taxi.DataAccess;
+
+namespace Taxi.WebApp.Controllers.Api
+{
+    public class CarModelController : ApiBaseController
+	{
+		public CarModelController(EntityContext context)
+			: base(context)
+		{ }
+    }
+}
