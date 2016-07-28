93edf821b81be6b069a3c4c59033987c8ba68268
alexroverandom <alexroverandom@gmail.com>
2016-07-29 00:21:05 +0300

    some changes of main page styles

--- Taxi.WebApp/Controllers/Api/DriverController.cs
+++ Taxi.WebApp/Controllers/Api/DriverController.cs
@@ -5,6 +5,8 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Taxi.DataAccess;
+using Taxi.Entities;
+using Taxi.WebApp.Models.Dtos;
 
 namespace Taxi.WebApp.Controllers.Api
 {
@@ -13,5 +15,38 @@ namespace Taxi.WebApp.Controllers.Api
 		public DriverController(EntityContext context)
 			: base(context)
 		{ }
+
+		[HttpPost]
+		public string LogIn(DriverLogInDto dto)
+		{
+			var driver = Db.Drivers.FirstOrDefault(d => d.Id == dto.DriverId);
+			if (driver == null)
+			{
+				throw new Exception($"There is no such driver with id = {dto.DriverId}");
+			}
+			var car = Db.Cars.FirstOrDefault(d => d.Id == dto.CarId);
+			if (car == null)
+			{
+				throw new Exception($"There is no such car with id = {dto.CarId}");
+			}
+
+			var cab = Db.Cabs.FirstOrDefault(c => c.DriverId == dto.DriverId && c.CarId == dto.CarId);
+			if (cab == null)
+			{
+				cab = new Cab
+				{
+					Car = car,
+					Driver = driver
+				};
+				Db.Cabs.Add(cab);
+			}
+			cab.Coords = new GeoCoordinates
+			{
+				Lattitude = dto.Lattitude,
+				Longitude = dto.Longitude
+			};
+			Db.SaveChanges();
+			return "Ok";
+		}
     }
 }
---
+++ Taxi.WebApp/Models/Dtos/DriverLogInDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Taxi.WebApp.Models.Dtos
+{
+	public class DriverLogInDto
+	{
+		public string DriverId { get; set; }
+		public int CarId { get; set; }
+		public double Lattitude { get; set; }
+		public double Longitude { get; set; }
+	}
+}
